246774c110985b8871d9958bedd6ee5399677ca3
Farid Huseynov <hferid23@gmail.com>
2025-02-13 14:22:11 +0100

    Add files via upload

---
+++ ders8/calisma1.py
@@ -0,0 +1,11 @@
+"""
+Müxtəlif şəhərlərin koordinatlarını (enlik, uzunluq) saxlayan tuple-lardan ibarət bir siyahı yaradin. Elə kod yazın ki:
+
+- Bütün şəhərlərin orta enliyini hesablasın.
+- Siyahı boş olarsa, None qaytarsın.
+
+Təlimatlar:
+- Koordinatlarla bir tuple siyahısı təyin edin. Mes. koordinatlar = [(12.3435, -40.3453), (35,5754, -34.5773) ...]
+- Orta enliyi hesablayib yazdirin.
+- Maksimum uzunluga malik koordinati (!) yazdirin.
+"""
