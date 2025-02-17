29dd110073761b3b7ba961409f4e175428d99a2a
Farid Huseynov <hferid23@gmail.com>
2025-02-17 15:07:59 +0100

    Add new lesson

---
+++ ders9/main.py
@@ -0,0 +1,28 @@
+"""
+FUNKSİYALAR
+def açar sözü
+"""
+
+"""
+Parametrlər
+Defolt dəyər
+"""
+
+"""
+*args - list kimi davranış
+**kwargs - dict kimi davranış
+"""
+
+"""
+return açar sözü
+çoxsaylı return dəyəri
+"""
+
+"""
+lambda funksiyaları (ananonim funksiyalar)
+a = lambda b,c: b*c
+"""
+
+"""
+global dəyişkənlər
+"""
