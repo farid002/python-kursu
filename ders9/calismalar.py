a3eaf66a48ed1f6cd2e32c9121e7eaec15db5dbe
Farid Huseynov <hferid23@gmail.com>
2025-02-17 15:11:56 +0100

    Create calismalar.py

---
+++ ders9/calismalar.py
@@ -0,0 +1,14 @@
+"""
+Tapşırıq 1: Funksiya və Parametrlər
+Bir funksiya yazın salamla(ad), hansı ki, istifadəçidən adını parametr kimi qəbul edir və onu salamlayır (heç nə qaytarmır).
+"""
+
+"""
+Tapşırıq 2: İki Ədədin Cəmi
+İki ədədi qəbul edib onların cəmini qaytaran bir funksiya yaradın.
+"""
+
+"""
+Tapşırıq 3: Faktoriyal Hesablama
+Bir funksiya yazın faktoriyal(n), hansı ki, verilmiş n ədədinin faktoriyalını hesablayır və qaytarır.
+"""
