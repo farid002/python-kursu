58760d488427c8fb7a13b62aaeac87bfbbe563e3
Farid Huseynov <hferid23@gmail.com>
2025-04-07 16:07:48 +0200

    Create main.py

---
+++ ders21/main.py
@@ -0,0 +1,25 @@
+"""
+Git - GitHub - GitLab
+Terminler:
+  Repository - repo
+  Branch
+  Clone
+  Pull
+  Push
+  Commit
+  Pull Request
+  Merge
+  Conflict
+
+Lazimli git commandlari:
+  git clone
+  git add
+  git commit
+  git push
+  git log
+  git checkout
+  git status
+  git merge
+
+.gitignore
+"""
