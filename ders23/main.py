4e684050ba506b2c50df5f74958de55a923a60ff
Farid Huseynov <hferid23@gmail.com>
2025-04-13 22:51:31 +0200

    Add next lesson

---
+++ ders23/main.py
@@ -0,0 +1,18 @@
+"""
+month = 5
+match month:
+    case 1:
+        print("January")
+    case 5:
+        print("February")
+    case _:
+        print("No Match")
+"""
+
+"Dict"
+
+"lambda"
+
+"kwargs"
+
+"args"
